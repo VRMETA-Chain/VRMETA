74d3fa4a015c22b5cecb64e4d7b3a97eeaa752f4
VRMETA-Chain <102953797+VRMETA-Chain@users.noreply.github.com>
2022-04-14 17:31:53 +0900

    Update Portal.sol

--- solidity/Portal.sol
+++ solidity/Portal.sol
@@ -5,34 +5,33 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Portal is Ownable {
     
-    IERC20 public vrmeta;
+    Vrmeta public vrmeta;
 
     constructor(address _vrmeta) {
-        vrmeta = IERC20(_vrmeta);
+        vrmeta = Vrmeta(_vrmeta);
     }
     
     event ClaimCreated(bytes32 hash, address who, uint amount);
+    event Processed(uint amountBurned);
 
     mapping(address=>uint) public balance;
 
-    function hashMirror(bytes memory hash, address who, uint amount) public onlyOwner returns(bytes32) {
-        bytes32 hashResponse = sha256(hash);
-        emit ClaimCreated(hashResponse, who, amount);
-        return hashResponse;
-    }
-
-    function deposit(uint amount) public {
-        vrmeta.transferFrom(msg.sender, address(this), amount);
-        balance[msg.sender] += amount;
+    function process(address who, uint amount) public onlyOwner {
+        vrmeta.burn(amount);
+        balance[who] -= amount;
+        emit Processed(amount);
     }
 
      function createClaim(bytes memory pw, uint256 amount) public {
+        require (vrmeta.balanceOf(msg.sender) >= amount);
+        vrmeta.transferFrom(msg.sender, address(this), amount);
+        balance[msg.sender] += amount;
         bytes32 claimHash = createPw(pw);
         emit ClaimCreated(claimHash, msg.sender, amount); 
         
     }
 
-     function createPw(bytes memory _pw) public view returns(bytes32){
+     function createPw(bytes memory _pw) public pure returns(bytes32){
        /// bytes32 pw = stringToBytes32(_pw); 
       return sha256(_pw);  
       
