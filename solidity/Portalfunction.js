9c0ecf19a9a341ad3526f78291d604d7722e7579
VRMETA-Chain <102953797+VRMETA-Chain@users.noreply.github.com>
2022-04-14 17:00:56 +0900

    Update Portalfunction.js

--- solidity/Portalfunction.js
+++ solidity/Portalfunction.js
@@ -2,22 +2,21 @@ async function processPortalHash() {
 	const api = await ApiPromise.create({ provider });
   const keyring = new Keyring({ type: 'sr25519' });
   const alice =  keyring.addFromUri('//Alice');
+  const eventArr = [];
  await api.tx.chainless.hashEmitted( 1000, "0x32")
   .signAndSend(alice, (result) => {
+   
      if (result.isInBlock) {
-     
         console.log('Events:');
-        result.events.forEach(({ event: { data, method, section }, phase }) => {
-    
-          console.log('\t', phase.toString(), `: ${section}.${method}`, data[0].toString());
-         
-          const hash = data[0].toString();
-         // currentHash.push(hash);
-          console.log(hash);
+        result.events.forEach(({ event, phase }) => {
+  
+         eventArr.push(event.data[0].toString());
         });
       }else if (result.status.isFinalized) {
-      console.log('finalized');
-      
+      console.log(eventArr[0]);
+      io.emit('processes hash', eventArr[0]);
 
     }
   });
+	
+}
